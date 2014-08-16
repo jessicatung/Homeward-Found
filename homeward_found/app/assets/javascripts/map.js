d20397d1c74b05aac5438c3120946a9b268a7501
Ernie Salazar <ehsalazar75@gmail.com>
2014-08-16 00:08:52 -0700

    Moved map js to new file

---
+++ homeward_found/app/assets/javascripts/map.js
@@ -0,0 +1,142 @@
+
+$(document).ready(function() {
+  var mapModel = new MapModel();
+  var mapController = new MapController(mapModel);
+  mapController.getLocation();
+});
+
+function MapController(model){
+  this.model = model
+}
+
+MapController.prototype = {
+  getLocation: function(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.initialize);
+    } else {
+      console.log( "Geolocation is not supported by this browser.");
+    }
+  },
+
+  initialize: function(position){
+    var currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+    var mapOptions = {
+      zoom: 13,
+      center: currentLocation
+    };
+
+    map = new google.maps.Map(document.getElementById('my_map'), mapOptions);
+    // debugger
+    this.model.drop();
+  }
+}
+
+function MapModel(){
+  var map;
+  var markers = [];
+  var iterator = 0;
+  var lostings = [
+  new google.maps.LatLng(37.7846330,-122.3974140),
+  new google.maps.LatLng(37.7959230,-122.3920520),
+  new google.maps.LatLng(37.7698250,-122.4667820),
+  ];
+  var self = this;
+
+  this.addMarker = function(){
+    markers.push(new google.maps.Marker({
+      position: lostings[iterator],
+      map: map,
+      draggable: false,
+      animation: google.maps.Animation.DROP
+    }));
+    iterator++;
+  };
+  this.drop = function(){
+    for (var i = 0; i < lostings.length; i++) {
+      setTimeout(function() {
+        self.addMarker();
+      }, i * (200 * i));
+    }
+  };
+}
+
+
+// =========== Original Code ==============================
+
+// $(document).ready(function() {
+
+//   var lostings = [
+//   new google.maps.LatLng(37.7846330,-122.3974140),
+//   new google.maps.LatLng(37.7959230,-122.3920520),
+//   new google.maps.LatLng(37.7698250,-122.4667820),
+//   ];
+
+//   var markers = [];
+//   var iterator = 0;
+
+//   var map;
+
+// //------
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(initialize);
+//   } else {
+//     console.log( "Geolocation is not supported by this browser.");
+//   }
+// }
+
+// function initialize(position) {
+//   console.log('HERE IS THE LOCATION')
+//   console.log(position)
+//   var currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+//   console.log(position.coords.longitude)
+//   var mapOptions = {
+//     zoom: 13,
+//     center: currentLocation
+//   };
+
+//   map = new google.maps.Map(document.getElementById('my_map'),
+//     mapOptions);
+
+//   drop();
+
+//   var allowedBounds = new google.maps.LatLngBounds(
+//     new google.maps.LatLng(37.717462, -122.541557),
+//     new google.maps.LatLng(37.817099, -122.378865)
+//     );
+
+//   var lastValidCenter = map.getCenter();
+
+//   google.maps.event.addListener(map, 'center_changed', function() {
+//     if (allowedBounds.contains(map.getCenter())) {
+//       lastValidCenter = map.getCenter();
+//       return;
+//     }
+//     map.panTo(lastValidCenter);
+//   });
+// }
+
+// // -----
+
+// function drop() {
+//   for (var i = 0; i < lostings.length; i++) {
+//     setTimeout(function() {
+//       addMarker();
+//     }, i * (200 * i));
+//   }
+// }
+
+// function addMarker() {
+//   markers.push(new google.maps.Marker({
+//     position: lostings[iterator],
+//     map: map,
+//     draggable: false,
+//     animation: google.maps.Animation.DROP
+//   }));
+//   iterator++;
+// }
+
+// getLocation();
+
+// });
+
