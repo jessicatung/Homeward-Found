a7e95cf83ab25ba9e19f045068f403678fdebbe3
Jeuel Wilkerson <jeuel@Jeuels-MacBook-Pro.local>
2014-08-15 20:38:01 -0700

    partially working

--- homeward_found/db/schema.rb
+++ homeward_found/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815181023) do
+ActiveRecord::Schema.define(version: 20140816010450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20140815181023) do
     t.date     "date_lost"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "sightings", force: true do |t|
@@ -46,6 +50,10 @@ ActiveRecord::Schema.define(version: 20140815181023) do
     t.boolean  "found"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: true do |t|
