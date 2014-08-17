c421e28c2cc44d6ffb5b2bd58fea873b4d9ba739
John Akers <johnakersemail@gmail.com>
2014-08-17 14:49:59 -0700

    basic login form on load not connected

---
+++ homeward_found/app/assets/javascripts/login.js
@@ -0,0 +1,21 @@
+$( document ).ready( function () {
+
+  // wait 2 seconds then call popup
+  setTimeout(popup, 2000);
+
+  // show form div
+  function popup() {
+    $("#logindiv").css("display", "block")
+  }
+
+  // when click on the login button, form goes away
+  $("#loginbtn").click( function () {
+
+    // // AJAX call needs to go here, sending off user info
+
+    $("#logindiv").remove()
+
+    // // may need validations within JS ???
+
+  });
+});
