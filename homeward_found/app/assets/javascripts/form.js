5b5578ae4a2a31472489df92d5004e2148c9b357
acreilly <acreilly3@gmail.com>
2014-08-17 15:01:26 -0700

    last commit incorrect, now working

--- homeward_found/app/assets/javascripts/form.js
+++ homeward_found/app/assets/javascripts/form.js
@@ -1,3 +1,36 @@
+function FormModel(){
+  this.catBreeds = []
+  this.dogBreeds = []
+}
+
+FormModel.prototype = {
+  animalCheck: function(){
+    var animalType = $("#losting_animal_type").val()
+
+    if(animalType === "cat"){
+      console.log(animalType)
+      self.catSelect()
+    } else if (animalType === "dog"){
+
+    }
+  },
+  catSelect: function(){
+    debugger
+    if($("#losting_animal_type").val() === "cat"){
+
+    $.ajax({
+      method: "GET",
+      url: "http://api.petfinder.com/breed.list?key=" + ENV['PET_FINDER_API_KEY'] + "&animal=cat&format=json"
+    }).done(function(data){
+      debugger
+    })
+
+    }
+  },
+  addBreeds: function(data){
+
+  }
+}
 
 
 function FormController(model){
@@ -5,6 +38,8 @@ function FormController(model){
   }
 FormController.prototype = {
   initialize: function(){
+    // this.animalCheck()
+    // this.animalCheck().bind(this.model)
     this.changeCheck()
   },
 
@@ -24,36 +59,28 @@ FormController.prototype = {
 
 }
 
-// function FormModel(){
-//   this.catBreeds = []
-//   this.dogBreeds = []
+
+
+// if ($("#losting_animal_type").val() === "cat"){
+// console.log("cat")
+// } else if ($("#losting_animal_type").val() == "dog"){
+
 // }
 
-// FormModel.prototype = {
-//   animalCheck: function(){
-//     var animalType = $("#losting_animal_type").val()
 
+// $("#losting_animal_type").on("CustomEvent", function(event){
+//     var animalType = $("#losting_animal_type").val()
 //     if(animalType === "cat"){
 //       console.log(animalType)
-//       self.catSelect()
-//     } else if (animalType === "dog"){
-
-//     }
-//   },
-//   catSelect: function(){
 //     debugger
-//     if($("#losting_animal_type").val() === "cat"){
-
-//     $.ajax({
-//       method: "GET",
-//       url: "http://api.petfinder.com/breed.list?key=" + ENV['PET_FINDER_API_KEY'] + "&animal=cat&format=json"
-//     }).done(function(data){
-//       debugger
+//       return animalType
+//     } else if (animalType === "dog"){
+//       console.log(animalType)
+//       return animalType
+//     }
 //     })
 
-//     }
-//   },
-//   addBreeds: function(data){
-
-//   }
-// }
+//     $("#losting_animal_type").click(function(){
+//       console.log("hello")
+//       $("#losting_animal_type").trigger("CustomEvent")
+//     })
