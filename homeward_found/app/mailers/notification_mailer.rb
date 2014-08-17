723894927acfdce0a84e99f20a5541bb4198791e
Steven Leu <steven.c.leu@gmail.com>
2014-08-17 12:43:32 -0700

    adds NotificationMailer class with method to email specified user creates HTML and text templates for mailing

---
+++ homeward_found/app/mailers/notification_mailer.rb
@@ -0,0 +1,9 @@
+class NotificationMailer < ActionMailer::Base
+  default from: 'pet_locator@homewardfound.com'
+
+  def possible_match_notification(user)
+    @user = user
+    @url = 'http://homeward-found.herokuapp.com'
+    mail(to: @user.email, subject: 'Homeward Found: Someone may have located your pet!')
+  end
+end
