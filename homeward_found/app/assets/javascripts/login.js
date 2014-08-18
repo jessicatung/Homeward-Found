8972e79f552062a6661a12ecd0f7838c484f02d1
John Akers <johnakersemail@gmail.com>
2014-08-17 17:27:48 -0700

    Login and Signup form onload is functional though not styled

--- homeward_found/app/assets/javascripts/login.js
+++ homeward_found/app/assets/javascripts/login.js
@@ -1,11 +1,11 @@
 $( document ).ready( function () {
 
   // wait 2 seconds then call popup
-  setTimeout(popup, 2000);
+  setTimeout(popup, 500);
 
   // show form div
   function popup() {
-    $("#logindiv").css("display", "block")
+      $("#logindiv").css("display", "block")
   }
 
   // when click on the login button, form goes away
