ed66359ce904042f22eb32df3dcacc321463c645
acreilly <acreilly3@gmail.com>
2014-08-17 19:49:11 -0700

    some styling on login form

--- homeward_found/app/assets/javascripts/login.js
+++ homeward_found/app/assets/javascripts/login.js
@@ -1,4 +1,7 @@
 $( document ).ready( function () {
+  var loginModel = new LoginModel()
+  var loginController = new LoginController(loginModel)
+  loginController.initialize()
 
   // wait 2 seconds then call popup
   setTimeout(popup, 500);
@@ -19,3 +22,38 @@ $( document ).ready( function () {
 
   });
 });
+
+function LoginModel(){}
+LoginModel.prototype = {
+  displayLoginForm: function(){
+    $("#loginForm").css("display", "block");
+    $("#login").css("display", "none");
+    $("#signup").css("display", "none");
+    $(".goBack").css("display", "block");
+  },
+  displaySignupForm: function(){
+    $("#signupForm").css("display", "block");
+    $("#login").css("display", "none");
+    $("#signup").css("display", "none");
+    $(".goBack").css("display", "block");
+  },
+  goBack: function(){
+    $("#login").css("display", "block");
+    $("#signup").css("display", "block");
+    $(".goBack").css("display", "none");
+    $("#loginForm").css("display", "none");
+    $("#signupForm").css("display", "none");
+  }
+}
+
+function LoginController(model){
+  this.model = model;
+}
+
+LoginController.prototype = {
+  initialize: function(){
+    $("#login").on("click", this.model.displayLoginForm)
+    $("#signup").on("click", this.model.displaySignupForm)
+    $(".goBack").on("click", this.model.goBack)
+  }
+}
