2fc26707829021499f0e917e2aa4f56433eccdf2
Ernie Salazar <ehsalazar75@gmail.com>
2014-08-19 15:31:07 -0700

    Add model validations

--- homeward_found/app/models/sighting.rb
+++ homeward_found/app/models/sighting.rb
@@ -2,6 +2,7 @@ class Sighting < ActiveRecord::Base
   belongs_to :user
   has_attached_file :avatar, :style => {medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates :coat_color, :coat_length, :breed, :size, :event_date, presence: true
 
   def self.ordered_json
     order("event_date DESC").limit(10).to_json
