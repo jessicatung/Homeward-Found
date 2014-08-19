0f0c65f3a7b738b8603c4393e8c31735bc6d018b
acreilly <acreilly3@gmail.com>
2014-08-19 14:14:27 -0700

    organized code

--- homeward_found/app/assets/javascripts/map.js
+++ homeward_found/app/assets/javascripts/map.js
@@ -17,8 +17,6 @@ MapController.prototype = {
     this.markers.initializeMarkers(this.model.map)
     this.markers.setAllMap(this.model.map)
     this.markers.showMarkers(this.model)
-    // this.lostings.getLostings()
-    // this.lostings.addInitialLostingMarkers(this.model.map)
   }
 }
 
@@ -43,7 +41,6 @@ MapModel.prototype = {
     };
 
     this.map = new google.maps.Map($("#my_map")[0], mapOptions)
-    // this.addInitialMarkers(this.map)
     // this.setMapBounds()
 
   },
@@ -62,81 +59,5 @@ MapModel.prototype = {
       }
       map.panTo(lastValidCenter);
     });
-  }
-}
-
-// === LOSTINGS ===================
-
-function Lostings(){
-  this.animalArray = [];
-  this.lostingsMarkers = [];
-  this.lostingsInfo = [];
-  this.iterator = 0;
-}
-
-Lostings.prototype = {
-  getLostings: function(){
-    $.ajax({
-      method: "GET",
-      url: "/lostings/recent"
-    }).done(function(data){
-      for(var i = 0; i < data.length; i++){
-        this.animalArray.push(data[i])
-      }
-    }.bind(this))
-  },
-  getLostingInfo: function(){
-    var content = []
-    for(var i = 0; i < this.animalArray.length; i++){
-      var url = "/lostings/" + this.animalArray[i].id
-      iter = i
-      content.push($.ajax({
-        method: "get",
-        async: false,
-        url: url
-      }).done(function(data){
-        this.lostingsInfo[iter] = data
-      }.bind(this)))
-    }
-    return content
-  }
-}
-
-
-// === SIGHTINGS ===================
-
-
-function Sightings(){
-  this.animalArray = [];
-  this.sightingsMarkers = [];
-  this.sightingsInfo = [];
-  this.iterator = 0;
-}
-
-Sightings.prototype = {
-  getSightings: function(){
-    $.ajax({
-      method: "GET",
-      url: "/sightings/recent"
-    }).done(function(data){
-      for(var i = 0; i < data.length; i++){
-        this.animalArray.push(data[i])
-      }
-    }.bind(this))
-  },
-  getSightingInfo: function(){
-    var content = []
-    for(var i = 0; i < this.animalArray.length; i++){
-      var url = "/sightings/" + this.animalArray[i].id
-      iter = i
-      content.push($.ajax({
-        method: "get",
-        async: false,
-        url: url
-      }).done(function(data){
-        this.sightingsInfo[iter] = data
-      }.bind(this)))
-    }
-    return content
   }
 }
---
+++ homeward_found/app/assets/javascripts/sightings.js
@@ -0,0 +1,39 @@
+
+
+// === SIGHTINGS ===================
+
+
+function Sightings(){
+  this.animalArray = [];
+  this.sightingsMarkers = [];
+  this.sightingsInfo = [];
+  this.iterator = 0;
+}
+
+Sightings.prototype = {
+  getSightings: function(){
+    $.ajax({
+      method: "GET",
+      url: "/sightings/recent"
+    }).done(function(data){
+      for(var i = 0; i < data.length; i++){
+        this.animalArray.push(data[i])
+      }
+    }.bind(this))
+  },
+  getSightingInfo: function(){
+    var content = []
+    for(var i = 0; i < this.animalArray.length; i++){
+      var url = "/sightings/" + this.animalArray[i].id
+      iter = i
+      content.push($.ajax({
+        method: "get",
+        async: false,
+        url: url
+      }).done(function(data){
+        this.sightingsInfo[iter] = data
+      }.bind(this)))
+    }
+    return content
+  }
+}
