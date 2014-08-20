2df7a6f10c39ed5cd978f6369769bf53e7703bf7
acreilly <acreilly3@gmail.com>
2014-08-20 11:53:01 -0700

    refactored, new markers controller and view

--- homeward_found/app/assets/javascripts/initialize.js
+++ homeward_found/app/assets/javascripts/initialize.js
@@ -8,9 +8,11 @@ $(document).ready(function() {
 //  ---------------------------- MAP
   var mapModel = new MapModel();
   var lostings = new Lostings();
-  var sightings = new Sightings()
-  var markers = new Marker(lostings, sightings)
-  var mapController = new MapController(mapModel, markers);
+  var sightings = new Sightings();
+  var markerView = new MarkerView();
+  var markerModel = new Marker(lostings, sightings, markerView)
+  var markersController = new MarkersController(markerModel, markerView)
+  var mapController = new MapController(mapModel, markersController);
   mapController.getLocation();
   lostings.getLostings()
   sightings.getSightings()
