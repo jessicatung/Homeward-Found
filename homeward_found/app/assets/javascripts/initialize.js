1fd2d4a29b52e257ee2b30e836701f340b2b9688
acreilly <acreilly3@gmail.com>
2014-08-19 19:30:13 -0700

    soo aside nav is working, lostings is working

--- homeward_found/app/assets/javascripts/initialize.js
+++ homeward_found/app/assets/javascripts/initialize.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
   // var riverController = new RiverController(riverView);
   // riverController.startLostings();
 
-$("#new_losting").on("ajax:success", routeModel.lostingRiver);
+// $("#new_losting").on("ajax:success", routeModel.lostingRiver);
 
   //  ---------------------------- LOGIN
   var loginModel = new LoginModel()
@@ -37,7 +37,7 @@ $("#new_losting").on("ajax:success", routeModel.lostingRiver);
 
 
   $("#event-container").on("submit", "#new_losting", routeModel.createLosting)
-  // $("#event-container").on("submit", "#new_sighting", routeModel.createSighting)
+  $("#event-container").on("submit", "#new_sighting", routeModel.createSighting)
 
 });
 
@@ -52,59 +52,3 @@ $("#new_losting").on("ajax:success", routeModel.lostingRiver);
     var riverController = new RiverController(riverView);
     riverController.startSightings()
   }
-
-
-
-
-// $(document).ready(function() {
-//   //  ---------------------------- MAP
-//   var mapModel = new MapModel();
-//   var lostings = new Lostings();
-//   var sightings = new Sightings()
-//   var markers = new Marker(lostings, sightings)
-//   var mapController = new MapController(mapModel, markers);
-//   mapController.getLocation();
-//   lostings.getLostings()
-//   sightings.getSightings()
-//   // mapController.initialize();
-//   //  ---------------------------- ROUTES
-//   var routeModel = new RouteModel();
-//   var routeController = new RouteController(routeModel)
-//   routeController.initialize()
-
-//   $("#new_losting").on("ajax:success", routeModel.lostingRiver);
-
-//   //  ---------------------------- FORM
-//   // var formController = new FormController();
-//   // formController.initialize()
-//  var formController = new FormController()
-//   formController.initialize()
-// var routeModel = new RouteModel();
-//   var routeController = new RouteController(routeModel)
-//   routeController.initialize()
-
-// $("#event-container").on("submit", "#new_losting", routeModel.createLosting)
-//   //  ---------------------------- RIVER
-//   // var riverView       = new RiverView ();
-//   // var riverController = new RiverController ( riverView );
-//   // riverController.startLostings();
-
-
-//   //  ---------------------------- LOGIN
-//   var loginModel = new LoginModel()
-//   var loginController = new LoginController(loginModel)
-//   loginController.initialize()
-//   // wait 2 seconds then call popup
-//   setTimeout(popup, 500);
-//   // show form div
-//   function popup() {
-//       $("#logindiv").css("display", "block")
-//   }
-//   // when click on the login button, form goes away
-//   $("#loginbtn").click( function () {
-//     // // AJAX call needs to go here, sending off user info
-//     $("#logindiv").remove()
-//     // // may need validations within JS ???
-
-//   });
-// });
--- homeward_found/app/assets/javascripts/routes.js
+++ homeward_found/app/assets/javascripts/routes.js
@@ -3,10 +3,7 @@ function RouteModel(){}
 RouteModel.prototype = {
   lostingForm: function(e){
     e.stopPropagation()
-
-    // login form interupt
     $("#logindiv").css("display", "block");
-
     $.ajax({
       method: "get",
       url: "/lostings/new"
@@ -16,10 +13,7 @@ RouteModel.prototype = {
   },
   sightingForm: function(e){
     e.stopPropagation()
-
-    // login form interupt
     $("#logindiv").css("display", "block");
-
     $.ajax({
      method: "get",
      url: "/sightings/new"
@@ -33,15 +27,21 @@ RouteModel.prototype = {
    method: "get",
    url: "/lostings"
  }).done(function(data){
+  var riverView = new RiverView ();
+  var riverController = new RiverController ( riverView );
+  riverController.startLostings();
   $("#event-container").html(data)
 })
 },
 sightingRiver: function(e){
-  e.preventDefault()
+  e.stopPropagation()
   $.ajax({
    method: "get",
    url: "/sightings"
  }).done(function(data){
+  var riverView = new RiverView ();
+  var riverController = new RiverController ( riverView );
+  riverController.startSightings();
   $("#event-container").html(data)
 })
 },
@@ -76,123 +76,14 @@ function RouteController(model){
 RouteController.prototype = {
   initialize: function(){
     this.bindListeners()
-    // this.startLostings()
   },
   bindListeners: function(){
     $("#sighting").on("click", this.model.sightingForm);
     $("#lost").on("click", this.model.lostingForm);
     $(document).on('reloadLostings', this.model.lostingRiver);
+    $(document).on('reloadSightings', this.model.sightingRiver);
     $("#home").on("click", this.model.lostingRiver);
     $("#lost_side").on("click", this.model.lostingRiver);
     $("#sight_side").on("click", this.model.sightingRiver);
-    // $(document).on('reloadSightings',this.model.sightingRiver);
-    // $("#aside_nav").on("click", "a", this.model.riverLoad)
-  // $("#aside_nav").on("click", "#lost_side", this.model.lostingRiver)
-  // $("#aside_nav").on("click", "#sight_side", this.model.sightingRiver)
-    // $("#lost_side").on("click", this.model.lostingRiver);
-    // $("#sight_side").on("click", this.model.sightingRiver);
   }
 }
-
-// function RouteView(){}
-
-// RouteView.prototype = {
-//   render: function ( dataArray ) {
-//     $.each(dataArray,function(index, data) {
-//       var source = $( "#event-template" ).html();
-//       var template = Handlebars.compile( source )
-//       $( '#event-container' ).append(template(data))
-//     });
-//   }
-// }
-
-
-
-
-
-
-
-// function RouteModel(){}
-// RouteModel.prototype = {
-//   lostingForm: function(e){
-//     e.stopPropagation()
-//     $.ajax({
-//       method: "get",
-//       url: "/lostings/new"
-//     }).done(function(data){
-//       $("#event-container").html(data)
-//     })
-//   },
-//   sightingForm: function(e){
-//     e.stopPropagation()
-//     $.ajax({
-//      method: "get",
-//      url: "/sightings/new"
-//    }).done(function(data){
-//     $("#event-container").html(data)
-//   })
-//  },
-//  lostingRiver: function(e){
-//   e.stopPropagation()
-//   $.ajax({
-//    method: "get",
-//    url: "/lostings"
-//  }).done(function(data){
-//   $("#event-container").html(data)
-//   // $("#sight_side").css("background-color", "#d3d3d3");
-//   // $("#all_side").css("background-color", "#d3d3d3");
-//   // $("#lost_side").css("background-color", "white");
-// })
-// },
-// sightingRiver: function(e){
-//   e.stopPropagation()
-//   $.ajax({
-//    method: "get",
-//    url: "/sightings"
-//  }).done(function(data){
-//   $("#event-container").html(data)
-//   // $("#sight_side").css("background-color", "white");
-//   // $("#lost_side").css("background-color", "#d3d3d3");
-//   // $("#all_side").css("background-color", "#d3d3d3");
-// })
-// },
-// createLosting: function(e){
-//   e.preventDefault()
-//   e.stopPropagation()
-//   $.ajax({
-//     method: "post",
-//     url: "/lostings",
-//     data: $(this).serialize()
-//   }).done(function(){
-//     $(document).trigger('reloadLostings')
-//   })
-// },
-// createSighting: function(e){
-//   e.stopPropagation()
-//   $.ajax({
-//     method: "post",
-//     url: "/lostings"
-//   }).done(function(data){
-//   })
-// }
-// }
-
-// function RouteController(model){
-//   this.model = model;
-// }
-
-// RouteController.prototype = {
-//   initialize: function(){
-//     this.bindListeners()
-//   },
-//   bindListeners: function(){
-//     $(document).on('reloadLostings',this.model.lostingRiver)
-//     $("#sighting").on("click", this.model.sightingForm);
-//     $("#lost").on("click", this.model.lostingForm);
-//     $("#home").on("click", this.model.lostingRiver);
-//     $("#lost_side").on("click", this.model.lostingRiver);
-//     $("#sight_side").on("click", this.model.sightingRiver);
-//   }
-// }
-
-
