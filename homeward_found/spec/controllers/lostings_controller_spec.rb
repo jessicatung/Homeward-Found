9094b7f5c8616437bb876405a532db6f04dfd2d4
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-08-20 23:17:25 -0700

    Changed behavior of lostings controller specs. - change expect in lostings#recent to verify actual objects rather than length - and change second lostings#recent test to verify older lostings not included - in lostings#create, changed test to do creation through controller rather than driver code, added dummy session to verify logged in user.

--- homeward_found/spec/controllers/lostings_controller_spec.rb
+++ homeward_found/spec/controllers/lostings_controller_spec.rb
@@ -3,31 +3,51 @@ require 'spec_helper'
 # losting = FactoryGirl.create(:losting)
 
 describe LostingsController do
-  before :each do
-    20.times do
-      FactoryGirl.create(:losting)
-    end
-  end
   context '#recent' do
-    it 'returns the 10 most recent lostings' do
+    # Arrange - creating objects that will exist at time of request
+    before :each do
+      20.times do
+        FactoryGirl.create(:losting)
+      end
       get :recent
-      expect(JSON.parse(response.body).length).to eq(10)
+      
+    end
+    it 'returns the 10 most recent lostings' do
+      # making instance variable of what response SHOULD be
+      recent_lostings = Losting.ordered_json
+      # expect(JSON.parse(response.body).length).to eq(10)
+
+      # compare what is returned from route 
+      # to what we have in line 14
+      expect(response.body).to eq(recent_lostings)
     end
     it 'does not return older lostings' do
-      get :recent
-      expect(JSON.parse(response.body).length).to eq(10)
+      # limit 10 would not make sense if change factory from 20 
+      older_lostings = JSON.parse(Losting.order("event_date ASC").limit(10).to_json(methods: [:long_date]))
+      
+      # expect(JSON.parse(response.body).length).to eq(10)
+      older_lostings.each do |ol|
+        expect(JSON.parse(response.body)).to_not include(ol)       
+      end
     end
   end
   context '#index' do
+    # lostings page does not render anything?
     it 'should respond to an AJAX call that corresponds to the associated lostings' do
       get :index
       expect(response).to render_template(partial: "_index")
     end
   end
   context '#create' do
-  let(:user) {FactoryGirl.create(:user)}
+    let(:user) {FactoryGirl.create(:user)}
     it 'should only create a losting associated to a user' do
-      losting = user.lostings.create(pet_name: 'fido')
+      # dummy session
+      session[:user_id] = user.id
+      # added this, they didn't have an #Act
+      post :create, losting: FactoryGirl.attributes_for(:losting)
+
+      #losting = user.lostings.create(pet_name: 'fido')
+      losting = Losting.first
       expect(losting.user_id).to eq(user.id)
     end
   end
